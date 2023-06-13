d72e15f8491571e9f87ecfcefb0f348b01686de2
Quan Qing <qingbsd@gmail.com>
2023-06-12 21:12:27 -0700

    package...

---
+++ tests/test_import.py
@@ -0,0 +1,11 @@
+import sys
+import importlib
+sys.path.insert(1, 'c:/users/quanq/Documents/GitHub/pyqlab/src/Datafile')
+#import pyqlab
+# from pyqlab.Datafile import Dataset
+# from pyqlab.Signals import sigprocess
+# from pyqlab.Datafile import Datafile
+import dataset
+
+if __name__ == '__main__':
+    print("test OK!")
