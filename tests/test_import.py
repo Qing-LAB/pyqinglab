43c30d1ac7fcde4d08320a9e2289c75bff575fc1
Quan Qing <qingbsd@gmail.com>
2023-06-12 21:35:52 -0700

    now can use pip install -e .

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,11 +1,11 @@
-import sys
-import importlib
-sys.path.insert(1, 'c:/users/quanq/Documents/GitHub/pyqlab/src/Datafile')
-#import pyqlab
-# from pyqlab.Datafile import Dataset
-# from pyqlab.Signals import sigprocess
+# import sys
+# import importlib
+# sys.path.insert(1, 'C:/Users/quanq/Documents/GitHub/pyqlab')
+import pyqlab
+from pyqlab.dataset import *
+from pyqlab.sigprocess import *
 # from pyqlab.Datafile import Datafile
-import dataset
+
 
 if __name__ == '__main__':
     print("test OK!")
