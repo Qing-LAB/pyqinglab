4ff79b39f4f46c7ac19379ac334c4faaa35cddf4
Quan Qing <quan.qing@asu.edu>
2023-07-19 16:56:31 -0700

    datafile bugs

--- pyqlab/datafile.py
+++ pyqlab/datafile.py
@@ -35,28 +35,6 @@ def fig2nparray(fig: Figure, dpi: int = 300) -> np.array:
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return data
 
-def generate_attr_str(attrs: dict, sep: str = "\n", eq: str = ":") -> str:
-    """Generate a string that contains all attributes with the format {key1:value1\\nkey2:value2\\n...}, \
-        where the default equal op ':' and the separator string \\n can be changed by the user.
-    
-    Parameters:
-    attrs: dictionary containing pairs of name and property as attributes for saving
-    sep: string as separator between each pair of attribute
-    eq: string as equal sign between the key name and the property
-    
-    Returns:
-    string containing all attributes as formatted
-
-    Example:
-
-    attrs = {'key1': 'value1', 'key2':'value2'}
-    attrs_str = generate_attr_str(attrs)
-
-    """
-    str_list = []
-    for k, v in attrs.items():
-        str_list.append(f"{str(k)}{eq}{str(v)}")
-    return sep.join(str_list)
 
 def get_timestamp_now() -> str:
     """Generate a string as the timestamp for the current time, and replaces ':' with '_', \
@@ -68,8 +46,7 @@ def get_timestamp_now() -> str:
     Returns:
     string containing the current time with special characters replaced by '_'
     """
-    ct = datetime.datetime.now()
-    ct = ct.replace(":", "_").replace("-", "_").replace(" ", "-")
+    ct = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     return ct
 
 class Datafile:
@@ -91,30 +68,66 @@ class Datafile:
         data = np.array(f"Initiated by operator: [{operator}]. Note: [{note}]").astype(dt)
         self.save_as_dataset(data, dataset_name, '/pyqlab_log', '', overwrite=True)
 
+    def generate_attr_str(attrs: dict, sep: str = "\n", eq: str = ":") -> str:
+        """Generate a string that contains all attributes with the format {key1:value1\\nkey2:value2\\n...}, \
+            where the default equal op ':' and the separator string \\n can be changed by the user.
+
+        Parameters:
+        attrs: dictionary containing pairs of name and property as attributes for saving
+        sep: string as separator between each pair of attribute
+        eq: string as equal sign between the key name and the property
+
+        Returns:
+        string containing all attributes as formatted
+
+        Example:
+
+        attrs = {'key1': 'value1', 'key2':'value2'}
+        attrs_str = generate_attr_str(attrs)
+
+        """
+        str_list = []
+        for k, v in attrs.items():
+            str_list.append(f"{str(k)}{eq}{str(v)}")
+        return sep.join(str_list)
+
     def create_group(self, groupname: str, parentkey: str, note:str, grouptype: str="") -> bool:
         success = False
         p = PurePath(self.root_group, parentkey, groupname)
-        parent_key = p.parent
+        parent_key = str(p.parent)
         grp_key = p.name
 
         with h5py.File(self.fname, 'a') as f:
             try:
+                create_flag = False
                 try:
-                    parent = f[parent_key]
+                    test_grp = f[str(p)]
+                    if isinstance(test_grp, h5py.Group):
+                        create_flag = False
+                    else:
+                        raise Exception(f"Name {str(p)} already exist but not for a group. Cannot create.")
                 except KeyError:
-                    parent = f.create_group(parent_key)
-            
-                if isinstance(parent, h5py.Datagroup):
-                    grp = parent.create_group(grp_key)
-                    ct = get_timestamp_now()
-                    attr_str = Datafile.generate_attr_str(
-                        {
-                            "timestamp": ct,
-                            "note": note,
-                            "grouptype": grouptype,
-                        }
-                    )
+                    create_flag = True
+                
+                if create_flag:
+                    try:
+                        parent = f[parent_key]
+                    except KeyError:
+                        parent = f.create_group(parent_key)
+
+                    if isinstance(parent, h5py.Group):
+                        grp = parent.create_group(grp_key)
+                        ct = get_timestamp_now()
+                        attr_str = Datafile.generate_attr_str(
+                            {
+                                "timestamp": ct,
+                                "note": note,
+                                "grouptype": grouptype,
+                            }
+                        )
                     success = True
+                print("Group prepared")
+                
             except ValueError:
                 self.console.print(f"group {p} cannot be created...")
                 self.console.print_exception(max_frames=20)
@@ -128,28 +141,31 @@ class Datafile:
     ) -> bool:
         success = False
         p = PurePath(groupkey, name)
-        grp_key = p.parent
+        grp_key = str(p.parent)
         n_key = p.name
         
-        if self.create_group(grp_key, '/', ''):
-            with h5py.File(self.fname, "a") as f:
-                grp_key = PurePath(self.root_group, grp_key)
-                grp = f[grp_key]
-                if not isinstance(grp, h5py.Group):
-                    raise Exception(
-                        "Name {grp_key} already occupied. Failed to create a data group with that name"
-                    )
-                if (n_key in grp.keys()) and isinstance(grp[n_key], h5py.Dataset):
-                    if overwrite:
-                        del grp[n_key]
-                if n_key in grp.keys():
-                    raise Exception(
-                        "Cannot overwrite key {n_key} in group {grp_key} as dataset"
-                    )
-                dset = grp.create_dataset(n_key, data=data)
-                dt = h5py.string_dtype(encoding="utf-8")
-                dset.attrs.create("created_by_pyqlab", np.array(attr_str).astype(dt))
-                success = True
+        try:
+            if self.create_group(grp_key, '/', ''):
+                with h5py.File(self.fname, "a") as f:
+                    grp_key = str(PurePath(self.root_group, grp_key))
+                    grp = f[grp_key]
+                    if not isinstance(grp, h5py.Group):
+                        raise Exception(
+                            "Name {grp_key} already occupied. Failed to create a data group with that name"
+                        )
+                    if (n_key in grp.keys()) and isinstance(grp[n_key], h5py.Dataset):
+                        if overwrite:
+                            del grp[n_key]
+                    if n_key in grp.keys():
+                        raise Exception(
+                            "Cannot overwrite key {n_key} in group {grp_key} as dataset"
+                        )
+                    dset = grp.create_dataset(n_key, data=data)
+                    dt = h5py.string_dtype(encoding="utf-8")
+                    dset.attrs.create("created_by_pyqlab", np.array(attr_str).astype(dt))
+                    success = True
+        except Exception:
+            self.console.print_exception(max_frames=20)
         
         return success
 
@@ -234,17 +250,17 @@ class Datafile:
         if self.create_group(p, '/', note=note, grouptype='DataFrame'):
             df.to_hdf(self.fname, p, 'r+')
         
-    def save_data_hierarchy(self, data: dict, groupkey: str):
-        for k, v in enumerate(data):
-            if isinstance(v, dict):
-                self.save_hierarchy_data(v, grp_key)
-            else:
-                match type(v):
-                    case int:
-                        None
-                    case double:
-                        None
-                    case str:
-                        None
-                    case np.array:
-                        None
+    # def save_data_hierarchy(self, data: dict, groupkey: str):
+    #     for k, v in enumerate(data):
+    #         if isinstance(v, dict):
+    #             self.save_hierarchy_data(v, grp_key)
+    #         else:
+    #             match type(v):
+    #                 case int:
+    #                     None
+    #                 case double:
+    #                     None
+    #                 case str:
+    #                     None
+    #                 case np.array:
+    #                     None
