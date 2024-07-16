e28153a0eca2d792c036ff5ab38a4a590ad297de
Quan Qing <qingbsd@gmail.com>
2024-07-16 11:35:42 -0700

    update hmm.py continue work

--- DataAnalysis/hmm.py
+++ DataAnalysis/hmm.py
@@ -6,6 +6,7 @@ from sklearn.utils import check_random_state
 from hmmlearn import hmm, vhmm
 import scipy.signal
 from tqdm.autonotebook import tqdm
+import click
 
 def lowpass_butterfly(data, cutoff, fs, order=5):
     nyq = 0.5 * fs
@@ -14,11 +15,15 @@ def lowpass_butterfly(data, cutoff, fs, order=5):
     y = filtfilt(b, a, data)
     return y
 
+
 def prep_data(pathname, filename, bandwidth):
     raw_data = xr.load_dataarray(os.path.join(pathname, filename))
     sampling_rate = raw_data.sampling_rate
-    if bandwidth > sampling_rate/2:
-        print(f"error: required bandwidth {bandwidth} is higher than what's allowed by the original sampling rate {sampling_rate}.")
+    if bandwidth > sampling_rate / 2:
+        print(
+            f"error: required bandwidth {bandwidth} is higher than what's allowed by the original sampling rate {sampling_rate}."
+        )
+
 
 # Train a suite of models, and keep track of the best model for each
 # number of states, and algorithm
@@ -27,35 +32,39 @@ def try_fit(data, num_inits=5, num_states=range(1, 10), verbose=False):
     best_models = collections.defaultdict(dict)
     for n in tqdm(num_states):
         for i in tqdm(range(num_inits)):
-            vi = vhmm.VariationalGaussianHMM(n,
-                                             n_iter=2000,
-                                             covariance_type="full",
-                                             implementation="scaling",
-                                             tol=1e-6,
-                                             random_state=rs,
-                                             verbose=verbose)
+            vi = vhmm.VariationalGaussianHMM(
+                n,
+                n_iter=2000,
+                covariance_type="full",
+                implementation="scaling",
+                tol=1e-6,
+                random_state=rs,
+                verbose=verbose,
+            )
             vi.fit(sequences, lengths)
             lb = vi.monitor_.history[-1]
-            #print(f"Training VI({n}) Variational Lower Bound={lb} "
+            # print(f"Training VI({n}) Variational Lower Bound={lb} "
             #      f"Iterations={len(vi.monitor_.history)} ")
             if best_models["VI"].get(n) is None or best_scores["VI"][n] < lb:
                 best_models["VI"][n] = vi
                 best_scores["VI"][n] = lb
-            em = hmm.GaussianHMM(n,
-                                 n_iter=1000,
-                                 covariance_type="full",
-                                 implementation="scaling",
-                                 tol=1e-6,
-                                 random_state=rs,
-                                 verbose=verbose)
+            em = hmm.GaussianHMM(
+                n,
+                n_iter=1000,
+                covariance_type="full",
+                implementation="scaling",
+                tol=1e-6,
+                random_state=rs,
+                verbose=verbose,
+            )
             em.fit(sequences, lengths)
             ll = em.monitor_.history[-1]
-            #print(f"Training EM({n}) Final Log Likelihood={ll} "
+            # print(f"Training EM({n}) Final Log Likelihood={ll} "
             #      f"Iterations={len(vi.monitor_.history)} ")
             if best_models["EM"].get(n) is None or best_scores["EM"][n] < ll:
                 best_models["EM"][n] = em
                 best_scores["EM"][n] = ll
-    
+
     # Display the model likelihood/variational lower bound for each N
     # and show the best learned model
     for algo, scores in best_scores.items():
@@ -64,20 +73,25 @@ def try_fit(data, num_inits=5, num_states=range(1, 10), verbose=False):
         for n, score in scores.items():
             flag = "* <- Best Model" if score == best_score else ""
             print(f"{algo}({n}): {score:.4f}{flag}")
-    
+
         print(f"Best Model {algo}")
         best_model = best_models[algo][best_n]
         print(best_model.transmat_)
         print(best_model.means_)
         print(best_model.covars_)
-    return {"EM_models": best_models["EM"], "VI_models":best_models["VI"], "EM_scores": best_scores["EM"], "VI_scores":best_scores["VI"]}
+    return {
+        "EM_models": best_models["EM"],
+        "VI_models": best_models["VI"],
+        "EM_scores": best_scores["EM"],
+        "VI_scores": best_scores["VI"],
+    }
 
 
 def sort_matrix(original_m, original_t):
     def switch_element(m, t, from_idx, to_idx):
         old_t = t
         old_m = m
-        for (i1, i2) in zip(from_idx, to_idx):
+        for i1, i2 in zip(from_idx, to_idx):
             new_t = np.zeros_like(t)
             new_m = np.zeros_like(m)
             for i in range(len(m)):
@@ -103,11 +117,11 @@ def sort_matrix(original_m, original_t):
 
     switch_from = []
     switch_to = []
-    m=original_m.copy()
+    m = original_m.copy()
 
     for i in range(len(m)):
         min_idx = i
-        for j in range(i+1, len(m)):
+        for j in range(i + 1, len(m)):
             if m[min_idx] > m[j]:
                 min_idx = j
         if i != min_idx:
@@ -118,3 +132,18 @@ def sort_matrix(original_m, original_t):
     new_m, new_t = switch_element(original_m, original_t, switch_from, switch_to)
     return new_m, new_t
 
+
+@click.command()
+@click.argument("directory_name")
+@click.argument()
+@click.argument("-l", "--log")
+def process_file():
+    pass
+
+@click.command()
+@click.argument("directory")
+def cli():
+    pass
+
+if __name__ == "__main__":
+    cli()
